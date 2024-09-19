a264af2e3abb0810bcb0b68a05ce4c836b889b9a
jaquelins <ajaquelinelins082@gmail.com>
2024-09-19 16:46:21 -0300

    novos exercícios

--- dados-numericos.py
+++ dados-numericos.py
@@ -13,3 +13,13 @@ myValue=5j
 print(myValue)
 print(type(myValue))
 print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
